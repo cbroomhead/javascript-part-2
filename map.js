714742dde936fd1107914303aca6d767d933ee18
Celine Broomhead <celine.broomhead@gmail.com>
2016-07-07 02:47:15 +0000

    I can output sandgit add map.js

--- map.js
+++ map.js
@@ -1,22 +1,3 @@
-/*var tile = { 
-if (tile === 'grass'){
-       
-   }
-   if (tile === 'water'){
-       
-   }
-   if (tile === 'sand'){
-       
-   }
-};
-
-var Map =[{tile:null}];
-
-function Maps (){
-   
-}   
-*/
-
 function Tile (type){
   this.type  = type;
   this.isWalkable = function(type){
@@ -29,19 +10,30 @@ function Tile (type){
   }
 };
 
-function Map(heigth, width){
-    this.height = heigth;
+function Map(height, width){
+    this.height = height;
     this.width = width;
     var bigArray =[];
-    for (var i=1; i < heigth; i++){
+    for (var i=1; i < height; i++){
         var littleArray =[];
             for (var j = 0 ; j < width ; j++){
                 littleArray.push(new Tile);
+                littleArray.type = Math.random();
+                if (Math.random() < 0.33){
+                    return "sand";
+                }
+                else if (0.33 <= Math.random() < 0.66){
+                    return "water";
+                }
+                else {
+                    return "grass";
+                }
             }
         bigArray.push(littleArray);
     }
 };
 
+console.log(Map(34, 67));
 
 
    
